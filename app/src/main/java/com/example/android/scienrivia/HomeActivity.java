4fa34458ab237fecd78affa7e9747af99960dd22
Akshay <axr151030@utdallas.edu>
2017-12-25 12:07:13 -0600

    Added Home screen picture Changed default orientation to portrait mode Changed the App mode to immersive

--- app/src/main/java/com/example/android/scienrivia/HomeActivity.java
+++ app/src/main/java/com/example/android/scienrivia/HomeActivity.java
@@ -1,13 +1,23 @@
 package com.example.android.scienrivia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View showAndHideBars = findViewById(R.id.mylayout);
+
+        showAndHideBars.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
     }
 }
